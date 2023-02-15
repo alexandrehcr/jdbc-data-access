693e04a8dbf7cc38573c4a0413d938e9cd2d885a
Alexandre Rocha <alexandrehc.rocha@gmail.com>
2023-02-15 08:12:50 -0300

    Implement SellerDaoJDBC::insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,21 +14,22 @@ public class Program {
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        System.out.println("===== TEST 1: SellerDao::findById() =====");
+        System.out.println("===== TEST 1: SellerDao::findById =====");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
-
-        System.out.println("\n===== TEST 2: SellerDao::findByDepartment() =====");
+        System.out.println("\n===== TEST 2: SellerDao::findByDepartment =====");
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
         list.forEach(System.out::println);
 
-        System.out.println("\n===== TEST 3: SellerDao::findAll() =====");
+        System.out.println("\n===== TEST 3: SellerDao::findAll =====");
         list = sellerDao.findAll();
         list.forEach(System.out::println);
 
-
-
+        System.out.println("\n===== TEST 4: SellerDao::insert =====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", LocalDate.now(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New seller id = " + newSeller.getId() + "\n");
     }
 }
