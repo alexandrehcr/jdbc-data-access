51d4564fa57a9ba237aea6b0ee619a93c11d33b2
Alexandre Rocha <alexandrehc.rocha@gmail.com>
2023-02-15 23:53:52 -0300

    Add DepartmentDaoJDBC and update DaoFactory
    Add DaoFactory::createDepartmentDao instantiating a DepartmentDaoJDBC.

---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,42 @@
+package model.dao.impl;
+
+import db.DB;
+import db.DbException;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+import java.sql.*;
+import java.util.List;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+    private Connection connection;
+
+    public DepartmentDaoJDBC(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public void insert(Department obj) {
+
+    }
+
+    @Override
+    public void update(Department obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return null;
+    }
+}
