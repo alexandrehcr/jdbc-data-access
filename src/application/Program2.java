763504c2c6592cf5984f064b1cece63c2d6c2221
Alexandre Rocha <alexandrehc.rocha@gmail.com>
2023-02-16 01:12:18 -0300

    Implement DepartmentDaoJDBC::findAll

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -4,6 +4,8 @@ import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
+import java.util.List;
+
 public class Program2 {
     public static void main(String[] args) {
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
@@ -13,17 +15,20 @@ public class Program2 {
 //        departmentDao.insert(department);
 //        System.out.println("Inserted! New department id = " + department.getId() + ".");
 
-        System.out.println("\n===== TEST 2: DepartmentDaoJDBC::update =====");
-        Department department2 = departmentDao.findById(3);
-        department2.setName("Cleaning");
-        departmentDao.update(department2);
+//        System.out.println("\n===== TEST 2: DepartmentDaoJDBC::update =====");
+//        Department department2 = departmentDao.findById(3);
+//        department2.setName("Cleaning");
+//        departmentDao.update(department2);
 
 //        System.out.println("\n===== TEST 3: DepartmentDaoJDBC::deleteById =====");
 //        departmentDao.deleteById(6);
 
-        System.out.println("\n===== TEST 4: DepartmentDaoJDBC::findById =====");
-        department2 = departmentDao.findById(3);
-        System.out.println(department2);
+//        System.out.println("\n===== TEST 4: DepartmentDaoJDBC::findById =====");
+//        department2 = departmentDao.findById(3);
+//        System.out.println(department2);
 
+        System.out.println("\n===== TEST 5: DepartmentDaoJDBC::findAll =====");
+        List<Department> departments = departmentDao.findAll();
+        departments.forEach(System.out::println);
     }
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -7,7 +7,10 @@ import model.dao.DepartmentDao;
 import model.entities.Department;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
     private Connection connection;
@@ -102,6 +105,24 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public List<Department> findAll() {
-        return null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = connection.prepareStatement("SELECT * FROM department ORDER BY Name");
+            rs = st.executeQuery();
+
+            List<Department> departments = new ArrayList<>();
+            while (rs.next()) {
+                Department department = instantiateDepartment(rs);
+                departments.add(department);
+            }
+            return departments;
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+        }
     }
 }
