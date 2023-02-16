53617bd5b9e52b048a236e426e63f6e988c9e280
Alexandre Rocha <alexandrehc.rocha@gmail.com>
2023-02-16 02:36:46 -0300

    Change DepartmentDao tests
    Change tests made in Program2

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -5,30 +5,61 @@ import model.dao.DepartmentDao;
 import model.entities.Department;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Program2 {
     public static void main(String[] args) {
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+        Scanner sc = new Scanner(System.in);
 
-//        System.out.println("===== TEST 1: DepartmentDaoJDBC::insert =====");
-//        Department department = new Department(null, "Engineering");
-//        departmentDao.insert(department);
-//        System.out.println("Inserted! New department id = " + department.getId() + ".");
+        System.out.println("===== TEST 1: DepartmentDaoJDBC::findAll =====");
+        List<Department> departmentList = departmentDao.findAll();
+        departmentList.forEach(System.out::println);
 
-//        System.out.println("\n===== TEST 2: DepartmentDaoJDBC::update =====");
-//        Department department2 = departmentDao.findById(3);
-//        department2.setName("Cleaning");
-//        departmentDao.update(department2);
 
-//        System.out.println("\n===== TEST 3: DepartmentDaoJDBC::deleteById =====");
-//        departmentDao.deleteById(6);
+        System.out.println("\n===== TEST 2: DepartmentDaoJDBC::findById =====");
+        //! Toggle input
+        int searchDptID = 3;
+//        System.out.print("ID: ");
+//        searchDptID = sc.nextInt(); sc.nextLine();
+        Department foundDepartment = departmentDao.findById(searchDptID);
+        System.out.println(foundDepartment);
 
-//        System.out.println("\n===== TEST 4: DepartmentDaoJDBC::findById =====");
-//        department2 = departmentDao.findById(3);
-//        System.out.println(department2);
 
-        System.out.println("\n===== TEST 5: DepartmentDaoJDBC::findAll =====");
-        List<Department> departments = departmentDao.findAll();
-        departments.forEach(System.out::println);
+        System.out.println("\n===== TEST 3: DepartmentDaoJDBC::insert =====");
+        //! Toggle input
+        String insertedDepartmentName = "Human Resources";
+//        System.out.print("Department name: ");
+//        insDepartmentName = sc.nextLine();
+        Department insertedDepartment = new Department(null, insertedDepartmentName);
+        departmentDao.insert(insertedDepartment);
+        int insertedDptId = insertedDepartment.getId();
+        System.out.println("Inserted " + insertedDepartmentName + "! New department id = " + insertedDptId + ".");
+        updatedDepartmentTable();
+
+
+        System.out.println("\n===== TEST 4: DepartmentDaoJDBC::update =====");
+        //! Toggle input
+        insertedDepartmentName = "Human Resources and Administration";
+//        System.out.print("New name to the department you inserted: ");
+//        insDepartmentName = sc.nextLine();
+        insertedDepartment.setName(insertedDepartmentName);
+        departmentDao.update(insertedDepartment);
+        updatedDepartmentTable();
+
+
+        System.out.println("\n===== TEST 5: DepartmentDaoJDBC::deleteById =====");
+        departmentDao.deleteById(insertedDptId);
+        System.out.println("Deleted " + insertedDepartmentName);
+        updatedDepartmentTable();
+
+        sc.close();
+    }
+
+    private static void updatedDepartmentTable() {
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+        System.out.println("\nUpdated Department table: ");
+        List<Department> departmentList = departmentDao.findAll();
+        departmentList.forEach(System.out::println);
     }
 }
